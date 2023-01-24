f1839577b3335f1e35c2f990179bcf0e8002d4d9
developer-bot-hub <117781817+developer-bot-hub@users.noreply.github.com>
2023-01-24 15:52:48 -0300

    variables in PYTHON
    variables in PYTHON

---
+++ variables.py
@@ -0,0 +1,18 @@
+nombre = "thelma"
+nombre = "louise"
+print(nombre)
+numero = 10
+numero += 5
+numero -= 5
+print(numero)
+num = 1
+pelicula = "thelma y " + nombre
+print(pelicula)
+pelicula2 = f"thelma y {num}"
+print(pelicula2)
+print("thelma" in pelicula)
+print("Thelma" in pelicula)
+print("louise" not in pelicula2)
+print("louise" not in pelicula)
+print("ana" in pelicula)
+print("ana" not in pelicula)
