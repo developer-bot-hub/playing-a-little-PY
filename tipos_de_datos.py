386f3116f67f7ea040220a48d93eb70329e1baa6
developer-bot-hub <117781817+developer-bot-hub@users.noreply.github.com>
2023-01-26 21:32:32 -0300

    tipos de datos en PYTHON
    tipos de datos en PYTHON

---
+++ tipos_de_datos.py
@@ -0,0 +1,15 @@
+print("tipos de datos")
+"string"
+'string'
+"""string:
+    multiples
+    lineas"""
+'''string:
+    multiples
+    lineas'''
+20
+2.0
+True
+False
+lista = ["domingo","lunes","martes","miercoles"]
+print(lista[0])
