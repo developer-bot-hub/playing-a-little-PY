de111577e5a13f07114e6ac4585c4c0ae34169a3
developer-bot-hub <117781817+developer-bot-hub@users.noreply.github.com>
2023-01-28 16:11:59 -0300

    funciones integradas de PYTHON
    funciones integradas de PYTHON

---
+++ built_in_functions.py
@@ -0,0 +1,17 @@
+numeros = [2,4,6]
+numero_mas_alto = max(numeros)
+print(numero_mas_alto)
+numero_mas_bajo = min(numeros)
+print(numero_mas_bajo)
+
+numero = round(12.3333333,2)
+print(numero)
+
+resultado_bool = bool([3,5])
+print(resultado_bool)
+
+resultado_all = all([3,5])
+print(resultado_all)
+
+suma_total = sum(numeros)
+print(suma_total)
